77df410234028ea50bcc2c0bf8c5d3d7730c435c
Serdar AKYOL <serdarakyol55@outlook.com>
2022-10-28 16:20:00 +0200

    Add database

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -2,7 +2,26 @@ package com.example.demo.Student;
 
 import java.time.LocalDate;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table 
 public class Student {
+     @Id
+    @SequenceGenerator(
+        name = "student_sequence",
+        sequenceName = "student_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue(
+        strategy = GenerationType.SEQUENCE,
+        generator = "student_sequence"
+    ) 
     private Long id;
     private String name;
     private String email;
