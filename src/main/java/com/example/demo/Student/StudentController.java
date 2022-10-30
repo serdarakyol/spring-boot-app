37f2139a1a6c8a6f933f0e749ad7f7abc5938a50
Serdar AKYOL <serdarakyol55@outlook.com>
2022-10-30 15:39:11 +0100

    add delete and put requests

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -3,10 +3,14 @@ package com.example.demo.Student;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -17,7 +21,7 @@ public class StudentController {
     private final StudentService studentService;
     
     @Autowired
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService){
         this.studentService = studentService;
     }
 
@@ -29,6 +33,20 @@ public class StudentController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(@PathVariable("studentId") Long studentId){
+        studentService.deleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void updateStudent(
+        @PathVariable("studentId") Long studentId,
+        @RequestParam(required = false) String name,
+        @RequestParam(required = false) String email
+    ){
+        studentService.updateStudent(studentId, name, email);
     }
 
 }
--- src/main/java/com/example/demo/Student/StudentService.java
+++ src/main/java/com/example/demo/Student/StudentService.java
@@ -1,10 +1,12 @@
 package com.example.demo.Student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService {
@@ -28,4 +30,32 @@ public class StudentService {
 		studentRepository.save(student);
 		System.out.println(student);
     }
+
+	public void deleteStudent(Long studentId) {
+		boolean isExist = studentRepository.existsById(studentId);
+		if (!isExist) {
+			throw new IllegalStateException("Student with ID " + studentId + " doesn't exist");
+		}
+		studentRepository.deleteById(studentId);
+	}
+
+	@Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+		Student student = studentRepository.findById(studentId).orElseThrow(() -> new IllegalStateException("Student with ID " + studentId + " doesn't exist"));
+
+		if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)){
+			student.setName(name);
+		}
+
+		if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)){
+			Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+			// check if e-mail taken
+			if (studentOptional.isPresent()){
+				throw new IllegalStateException("E-mail taken");
+			}
+			student.setEmail(email);
+		}
+    }
+
+	
 }
