6ee45024cc2cb56bda8c8296dd97da5f9cc2b253
Serdar AKYOL <serdarakyol55@outlook.com>
2022-12-27 12:17:08 +0100

    wip

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,8 +31,8 @@ public class StudentController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Student>> getStudent() {
-        return ResponseEntity.ok(studentService.getStudents());
+    public List<Student> getStudent() {
+        return studentService.getStudents();
     }
 
     @GetMapping(path = "{studentId}")
