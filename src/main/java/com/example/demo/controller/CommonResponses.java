3055422bec99dac40b923b33f09ebce15072207d
Serdar AKYOL <serdarakyol55@outlook.com>
2023-01-19 13:55:34 +0100

    wip

--- src/main/java/com/example/demo/controller/CommonResponses.java
+++ src/main/java/com/example/demo/controller/CommonResponses.java
@@ -2,5 +2,5 @@ package com.example.demo.controller;
 
 public final class CommonResponses {
     public static final String emailTakenMsg= "E-mail is taken. Please add another E-mail";
-	public static final String emailNotValidMsg = "The E-mail is not valid. Please write a valid e-mail";
+    public static final String emailNotValidMsg = "The E-mail is not valid. Please write a valid e-mail";
 }
