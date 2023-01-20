0de09763d573165fbf2eab619980fa1feabef336
Serdar AKYOL <serdarakyol55@outlook.com>
2023-01-20 15:03:18 +0100

    Renamed parameters (#25)

--- src/main/java/com/example/demo/repository/StudentRepository.java
+++ src/main/java/com/example/demo/repository/StudentRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.entity.Student;
 
 @Repository
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    @Query("SELECT s FROM Student s WHERE s.email = ?1")
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+    @Query("SELECT s FROM Student s WHERE s.studentEmail = ?1")
     Optional<Student> findStudentByEmail(String email);
 }
--- src/main/java/com/example/demo/service/StudentService.java
+++ src/main/java/com/example/demo/service/StudentService.java
@@ -5,12 +5,10 @@ import java.util.List;
 import com.example.demo.entity.Student;
 
 public interface StudentService {
-
     public void addNewStudent(Student student);
-    public void deleteStudent(Long studentId);
-    public void updateStudent(Long studentId, String name, String email);
-    public Student getStudentById(Long studentId);
+    public void deleteStudent(int studentId);
+    public void updateStudent(int studentId, String name, String email);
+    public Student getStudentById(int studentId);
     public Student getStudentByEmail(String email);
     public List<Student> getStudents();
-
 }
