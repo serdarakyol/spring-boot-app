cba5a98e2583baf8ee05174bf5f323a8eefc8dd4
Serdar AKYOL <serdarakyol55@outlook.com>
2023-01-30 10:40:35 +0100

    Revert "Deleted project-dev (#32)"
    This reverts commit d9ada028fb92d5fa34a4c55715001512d95a9d0b.

---
+++ src/test/java/com/example/demo/repository/StudentRepositoryTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.repository;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import com.example.demo.entity.Student;
+
+@DataJpaTest
+public class StudentRepositoryTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    void testFindStudentByEmail() {
+        // Given
+        Student student = new Student(
+            "Serdar",
+            "serdarakyol55@outlook.com",
+            LocalDate.parse("2000-03-29")
+        );
+        studentRepository.save(student);
+        
+        // When
+        Student studentRecord = studentRepository.findStudentByEmail("serdarakyol55@outlook.com").get();
+
+        // Then
+        assertThat(studentRecord).isEqualTo(student);
+    }
+}
