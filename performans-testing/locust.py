feb20119fd57950b4158bc13a0d8c6becdf008f6
Serdar AKYOL <serdarakyol55@outlook.com>
2023-01-31 17:45:28 +0100

    wip

--- performans-testing/locust.py
+++ performans-testing/locust.py
@@ -18,7 +18,7 @@ class LoadTest(HttpUser):
     abstract = True
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.path_all = None
+        self.base_path = None
         self.saved_emails = []
 
     def generate_random_string(self, min_name_size=2, max_name_size=20) -> str:
@@ -33,21 +33,21 @@ class LoadTest(HttpUser):
 
     @task(1)
     def get_all(self):
-        self.client.get(url=self.path_all)
+        self.client.get(url=self.base_path)
 
-    @task
+    @task(1)
     def get_one_by_id(self):
         random_id = random.randint(1, len(self.saved_emails))
-        self.client.get(url=f"{self.path_all}/by-id/{random_id}")
+        self.client.get(url=f"{self.base_path}/by-id/{random_id}")
 
-    @task
+    @task(1)
     def get_one_by_email(self):
         random_email = random.choice(self.saved_emails)
-        self.client.get(url=f"{self.path_all}/by-email/{random_email}")
+        self.client.get(url=f"{self.base_path}/by-email/{random_email}")
 
     @task(100)
     def post_request(self):
-        self.client.post(url=self.path_all, json=self._generate_post_data(), headers=h)
+        self.client.post(url=self.base_path, json=self._generate_post_data(), headers=h)
 
     @task(2)
     def put_request(self):
@@ -57,15 +57,14 @@ class LoadTest(HttpUser):
 class TeacherProcess(LoadTest):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.path_all = "/api/v1/teacher"
+        self.base_path = "/api/v1/teacher"
 
     def on_start(self):
         for _ in range(2):
             generated_data = self._generate_post_data()
-            self.client.post(url=self.path_all,
+            self.client.post(url=self.base_path,
                              json=generated_data,
                              headers=h)
-            self.saved_emails.append(generated_data["teacherEmail"])
 
     def _generate_post_data(self) -> dict:
         request_data = {
@@ -86,25 +85,28 @@ class TeacherProcess(LoadTest):
             teacher_email = f"{self.generate_random_string()}@{self.generate_random_string()}.{self.generate_random_string()}"
             teacher_email = teacher_email.replace("@", "%40")
             teacher_id = str(random.randint(1, len(self.saved_emails)))
-            request_string = f"{self.path_all}/{teacher_id}?teacherName={teacher_name}&teacherEmail={teacher_email}"
+            request_string = f"{self.base_path}/{teacher_id}?teacherName={teacher_name}&teacherEmail={teacher_email}"
                 
             return request_string
         # this is added for handle None returns
-        return f"{self.path_all}/1?teacherName=ibjy&teacherEmail=et%40bedhaxkfv.fndxldvtrkdsjcjiwit"
+        return f"{self.base_path}/1?teacherName=ibjy&teacherEmail=et%40bedhaxkfv.fndxldvtrkdsjcjiwit"
 
 
 class StudentProcess(LoadTest):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.path_all = "/api/v1/student"
+        self.base_path = "/api/v1/student"
 
     def on_start(self):
         for _ in range(2):
             generated_data = self._generate_post_data()
-            self.client.post(url=self.path_all,
+            self.client.post(url=self.base_path,
                              json=generated_data,
                              headers=h)
-            self.saved_emails.append(generated_data["studentEmail"])
+    
+    def on_stop(self):
+        for email in self.saved_emails:
+            self.client.delete(url=f"{self.base_path}/by-email/{email}")
 
     def _generate_post_data(self) -> dict:
         request_data = {
@@ -125,8 +127,8 @@ class StudentProcess(LoadTest):
             student_email = f"{self.generate_random_string()}@{self.generate_random_string()}.{self.generate_random_string()}"
             student_email = student_email.replace("@", "%40")
             student_id = str(random.randint(1, len(self.saved_emails)))
-            request_string = f"{self.path_all}/{student_id}?studentName={student_name}&studentEmail={student_email}"
+            request_string = f"{self.base_path}/{student_id}?studentName={student_name}&studentEmail={student_email}"
 
             return request_string
         # this is added for handle None returns
-        return f"{self.path_all}/1?studentName=ibjy&studentEmail=et%40bedhaxkfv.fndxldvtrkdsjcjiwit"
+        return f"{self.base_path}/1?studentName=ibjy&studentEmail=et%40bedhaxkfv.fndxldvtrkdsjcjiwit"
