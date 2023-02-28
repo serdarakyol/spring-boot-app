0b031fd80b36e18d122ca72a671bd9fc64ba7b32
Serdar AKYOL <serdarakyol55@outlook.com>
2023-02-28 18:31:05 +0100

    made all requests are working

--- src/main/java/com/example/demo/serviceIml/StudentServiceImpl.java
+++ src/main/java/com/example/demo/serviceIml/StudentServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.demo.serviceIml;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,19 +25,74 @@ public class StudentServiceImpl implements StudentService{
 	private final String studentNotExistByIdMsg = "Student doesn't exist, student ID: ";
 	private final String studentNotExistByEmailMsg = "Student doesn't exist, student E-MAIL: ";
 
+	@Autowired
 	private final StudentRepository studentRepository;
 
-	/*
-	Note to me!!!
-	@RequiredArgsConstructor does the same thing like below code
-	@Autowired
-	public StudentService(StudentRepository studentRepository) {
-		this.studentRepository = studentRepository;
-	}
-	*/
 	@Override
-	public List<Student> getStudents() {
-		return studentRepository.findAll();
+	public void addNewStudent(Student student) {
+		Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getStudentEmail());
+		if (studentOptional.isPresent()) {
+			throw new BadRequestException(CommonResponses.emailTakenMsg);
+		}
+
+		// check if mail is valid
+		if (!Utils.isMailValid(student.getStudentEmail())){
+			throw new BadRequestException(CommonResponses.emailNotValidMsg);
+		}
+		studentRepository.save(student);
+	}
+
+	@Transactional
+	@Override
+	public void updateStudent(int studentId, String studentName, String studentEmail, LocalDate studentDOB) {
+		Student student = studentRepository.findById(studentId)
+				.orElseThrow(() -> new NotFoundException(studentNotExistByIdMsg + studentId));
+
+		// Student name processing
+		if (studentName == null || studentName.length() < 2 || Objects.equals(student.getStudentName(), studentName)) {
+			throw new BadRequestException(CommonResponses.nameNotValidMsg);
+		}
+		student.setStudentName(studentName);
+
+		// Student email processing
+		if (!Utils.isMailValid(studentEmail)) {
+			throw new BadRequestException(CommonResponses.emailNotValidMsg);
+		}
+
+		if (studentEmail == null || studentEmail.length() < 2 || Objects.equals(student.getStudentEmail(), studentEmail)) {
+			throw new BadRequestException(CommonResponses.emailNotValidMsg);
+		}
+
+		Optional<Student> studentOptional = studentRepository.findStudentByEmail(studentEmail);
+		// check if e-mail taken
+		if (studentOptional.isPresent()) {
+			throw new BadRequestException(CommonResponses.emailTakenMsg);
+		}
+		student.setStudentEmail(studentEmail);
+
+		// Student DOB processing
+		if (studentDOB == null || Objects.equals(studentDOB, student.getStudentDOB())){
+			throw new BadRequestException(CommonResponses.birthdayNotValidMsg);
+		}
+		student.setStudentDOB(studentDOB);
+	}
+
+	@Override
+	public void deleteStudentById(int studentId) {
+		boolean isStudentExist = studentRepository.existsById(studentId);
+		if (!isStudentExist) {
+			throw new NotFoundException(studentNotExistByIdMsg + studentId);
+		}
+		studentRepository.deleteById(studentId);
+	}
+
+	@Override
+	public void deleteStudentByEmail(String studentEmail) {
+		boolean isStudentExist = studentRepository.findStudentByEmail(studentEmail).isPresent();
+		if (!isStudentExist) {
+			throw new NotFoundException(studentNotExistByIdMsg + studentEmail);
+		}
+		studentRepository.deleteStudentByEmail(studentEmail);
 	}
 
 	@Override
@@ -55,60 +112,17 @@ public class StudentServiceImpl implements StudentService{
 	}
 
 	@Override
-	public void addNewStudent(Student student) {
-		Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getStudentEmail());
-		if (studentOptional.isPresent()) {
-			throw new BadRequestException(CommonResponses.emailTakenMsg);
-		}
-
-		// check if mail is valid
-		if (!Utils.isMailValid(student.getStudentEmail())){
-			throw new BadRequestException(CommonResponses.emailNotValidMsg);
-		}
-		studentRepository.save(student);
+	public List<Student> getStudents() {
+		return studentRepository.findAll();
 	}
-
-	@Override
-	public void deleteStudentById(int studentId) {
-		boolean isStudentExist = studentRepository.existsById(studentId);
-		if (!isStudentExist) {
-			throw new NotFoundException(studentNotExistByIdMsg + studentId);
-		}
-		studentRepository.deleteById(studentId);
-	}
-
-	@Override
-	public void deleteStudentByEmail(String studentEmail) {
-		boolean isStudentExist = studentRepository.findStudentByEmail(studentEmail).isPresent();
-		if (!isStudentExist) {
-			throw new NotFoundException(studentNotExistByIdMsg + studentEmail);
-		}
-
-		studentRepository.deleteStudentByEmail(studentEmail);
-	}
-
-	@Transactional
-	@Override
-	public void updateStudent(int studentId, String studentName, String studentEmail) {
-		Student student = studentRepository.findById(studentId)
-				.orElseThrow(() -> new NotFoundException(studentNotExistByIdMsg + studentId));
-
-		if (studentName != null && studentName.length() > 0 && !Objects.equals(student.getStudentName(), studentName)) {
-			student.setStudentName(studentName);
-		}
-
-		if (!Utils.isMailValid(studentEmail)) {
-			throw new BadRequestException(CommonResponses.emailNotValidMsg);
-		}
-
-		if (studentEmail != null && studentEmail.length() > 0 && !Objects.equals(student.getStudentEmail(), studentEmail)) {
-			Optional<Student> studentOptional = studentRepository.findStudentByEmail(studentEmail);
-			// check if e-mail taken
-			if (studentOptional.isPresent()) {
-				throw new BadRequestException(CommonResponses.emailTakenMsg);
-			}
-			student.setStudentEmail(studentEmail);
-		}
-	}
-
 }
+
+
+/*
+Note to me!!!
+@RequiredArgsConstructor does the same thing like below code
+@Autowired
+public StudentService(StudentRepository studentRepository) {
+	this.studentRepository = studentRepository;
+}
+*/
--- src/main/java/com/example/demo/serviceIml/TeacherServiceImpl.java
+++ src/main/java/com/example/demo/serviceIml/TeacherServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.demo.serviceIml;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -26,7 +27,6 @@ public class TeacherServiceImpl implements TeacherService {
 
     @Autowired
     private final TeacherRepository teacherRepository;
-    
 
     @Override
     public void addNewTeacher(Teacher teacher) {
@@ -36,7 +36,7 @@ public class TeacherServiceImpl implements TeacherService {
         }
 
         // check if mail is valid
-        if ( !Utils.isMailValid(teacher.getTeacherEmail()) ) {
+        if (!Utils.isMailValid(teacher.getTeacherEmail())) {
             throw new BadRequestException(CommonResponses.emailNotValidMsg);
         }
         teacherRepository.save(teacher);
@@ -44,31 +44,37 @@ public class TeacherServiceImpl implements TeacherService {
 
     @Transactional
     @Override
-    public void updateTeacherById(int teacherId, String teacherName, String teacherEmail){
-        Teacher teacher = teacherRepository.findById(teacherId).orElseThrow(
-            () -> new NotFoundException(teacherNotExistByIdMsg + teacherId)
-        );
+    public void updateTeacherById(int teacherId, String teacherName, String teacherEmail, LocalDate teacherDOB) {
+        Teacher teacher = teacherRepository.findById(teacherId)
+                .orElseThrow(() -> new NotFoundException(teacherNotExistByIdMsg + teacherId));
 
-        if (teacherName != null && teacherName.length() > 1 && !Objects.equals(
-            teacher.getTeacherName(), teacherName
-        )) {
-            teacher.setTeacherName(teacherName);
+        // Teacher name processing
+        if (teacherName == null || teacherName.length() < 2 || Objects.equals(teacher.getTeacherName(), teacherName)) {
+            throw new BadRequestException(CommonResponses.nameNotValidMsg);
         }
+        teacher.setTeacherName(teacherName);
 
+        // Teacher email processing
         if (!Utils.isMailValid(teacherEmail)) {
             throw new BadRequestException(CommonResponses.emailNotValidMsg);
         }
 
-        if (teacherEmail != null && teacherEmail.length() > 1 && !Objects.equals(
-            teacher.getTeacherEmail(), teacherEmail
-        )) {
-            Optional<Teacher> teacherOptional = teacherRepository.findTeacherByTeacherEmail(teacherEmail);
-            if (teacherOptional.isPresent()) {
-                throw new BadRequestException(CommonResponses.emailTakenMsg);
-            }
-
-            teacher.setTeacherEmail(teacherEmail);
+        if (teacherEmail == null || teacherEmail.length() < 2 || Objects.equals(teacher.getTeacherEmail(), teacherEmail)) {
+            throw new BadRequestException(CommonResponses.emailNotValidMsg);
         }
+
+        Optional<Teacher> teacherOptional = teacherRepository.findTeacherByTeacherEmail(teacherEmail);
+        // check if e-mail taken
+        if (teacherOptional.isPresent()) {
+            throw new BadRequestException(CommonResponses.emailTakenMsg);
+        }
+        teacher.setTeacherEmail(teacherEmail);
+
+        // Teacher DOB processing
+        if (teacherDOB == null || Objects.equals(teacherDOB, teacher.getTeacherDOB())) {
+            throw new BadRequestException(CommonResponses.birthdayNotValidMsg);
+        }
+        teacher.setTeacherDOB(teacherDOB);
     }
 
     @Override
@@ -77,7 +83,6 @@ public class TeacherServiceImpl implements TeacherService {
         if (!isExist) {
             throw new NotFoundException(teacherNotExistByIdMsg);
         }
-
         teacherRepository.deleteById(teacherId);
     }
 
@@ -87,24 +92,23 @@ public class TeacherServiceImpl implements TeacherService {
         if (!isTeacherExist) {
             throw new NotFoundException(teacherNotExistByIdMsg);
         }
-        
         teacherRepository.deleteTeacherByMail(teacherEmail);
     }
 
     @Override
-	public Teacher getTeacherById(int teacherId) {
-		Teacher teacher = teacherRepository.findById(teacherId)
-				.orElseThrow(() -> new NotFoundException(teacherNotExistByIdMsg + teacherId));
-		return teacher;
-	}
+    public Teacher getTeacherById(int teacherId) {
+        Teacher teacher = teacherRepository.findById(teacherId)
+                .orElseThrow(() -> new NotFoundException(teacherNotExistByIdMsg + teacherId));
+        return teacher;
+    }
 
     @Override
     public Teacher getTeacherByEmail(String teacherEmail) {
         Optional<Teacher> teacherOptional = teacherRepository.findTeacherByTeacherEmail(teacherEmail);
-		if (teacherOptional.isEmpty()) {
-			throw new NotFoundException(teacherNotExistByEmailMsg + teacherEmail);
-		}
-		return teacherOptional.get();
+        if (teacherOptional.isEmpty()) {
+            throw new NotFoundException(teacherNotExistByEmailMsg + teacherEmail);
+        }
+        return teacherOptional.get();
     }
 
     @Override
