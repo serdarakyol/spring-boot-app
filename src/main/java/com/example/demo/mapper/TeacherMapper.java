acad542ad8f9406bf4932a8f1e40bd85f5a7bbc0
Serdar AKYOL <serdarakyol55@outlook.com>
2023-03-01 12:51:16 +0100

    Added DTO to teacher

---
+++ src/main/java/com/example/demo/mapper/TeacherMapper.java
@@ -0,0 +1,18 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+import com.example.demo.dto.TeacherDTO;
+import com.example.demo.entity.Teacher;
+
+@Mapper(componentModel = "spring")
+public interface TeacherMapper {
+    TeacherMapper INSTANCE = Mappers.getMapper(TeacherMapper.class);
+
+    TeacherDTO teacherToDto(Teacher teacher);
+
+    List<TeacherDTO> teacherToDto(List<Teacher> students);
+}
