4ca15ba3a214aaae7b4820a6e900658b2a4a6d06
Serdar AKYOL <serdarakyol55@outlook.com>
2023-03-29 14:35:41 +0200

    Added unauthentication error message

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,12 +6,24 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+
 @Configuration
+@Log4j2
 public class SecurityConfig {
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
+        http.csrf().disable().authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated().and())
+                .httpBasic(Customizer.withDefaults()).exceptionHandling()
+                .authenticationEntryPoint((request, response, e) -> {
+                    log.error("Full authentication is required to access this resource");
+                    String json = String.format("{\"message\": \"%s\"}", e.getMessage());
+                    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+                    response.getWriter().write(json);
+                });
 
         return http.build();
     }
