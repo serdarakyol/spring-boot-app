7ed72a5ebd05bbb144cfb2cace6a6bb91a2ca230
Serdar AKYOL <serdarakyol55@outlook.com>
2023-03-31 12:18:55 +0200

    wip

--- src/main/java/com/example/demo/controller/CourseController.java
+++ src/main/java/com/example/demo/controller/CourseController.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -36,7 +36,7 @@ public class CourseController {
         return BodyResponses.CREATED;
     }
 
-    @PutMapping(path = "{courseId}")
+    @PatchMapping(path = "{courseId}")
     public String updateCourse(@PathVariable("courseId") String courseId, @RequestBody Course updateCourse) {
         courseServiceImpl.updateCourse(courseId, updateCourse);
 
