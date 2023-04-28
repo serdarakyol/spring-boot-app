f7ecb59c714a0ae227c704a0c0c30e03723585a9
Serdar AKYOL <serdarakyol55@outlook.com>
2023-04-28 13:02:55 +0200

    Removed memory DB for testing (#59)

--- src/test/java/com/example/demo/repository/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/repository/StudentRepositoryTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -18,6 +19,7 @@ import com.example.demo.entity.Student;
 
 @ExtendWith(MockitoExtension.class)
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class StudentRepositoryTest {
 
     @MockBean
